d0e4eb76f6306cd8674db7a6942577c91e71d4a3
Mustafa Şahin <mustafasahincontact@gmail.com>
2026-04-09 20:54:29 +0300

    Add files via upload

---
+++ src/app/deneme/page.tsx
@@ -0,0 +1,17 @@
+export default function Page() {
+  return (
+    <iframe 
+      src="https://sonusmazmatematik-github-io.vercel.app/deneme.html"
+      style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        border: 'none',
+        zIndex: 0
+      }}
+      title="Sayfa"
+    />
+  );
+}
