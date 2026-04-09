af7faac088d20adcd3d7d7b070839e713562ba47
Mustafa Şahin <mustafasahincontact@gmail.com>
2026-04-09 20:43:19 +0300

    Create page.tsx

---
+++ src/app/ders-notlari/page.tsx
@@ -0,0 +1,11 @@
+export default function DersNotlari() {
+  return (
+    <div style={{minHeight:'100vh'}}>
+      <iframe 
+        src="https://sonusmazmatematik-github-io.vercel.app/ders-notlari.html"
+        style={{width:'100%', height:'100vh', border:'none', display:'block'}}
+        title="Ders Notları"
+      />
+    </div>
+  );
+}
