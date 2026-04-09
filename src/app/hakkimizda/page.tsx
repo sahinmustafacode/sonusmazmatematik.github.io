d8b0c45f5703349e8ea005aa56e6e5e78b25db3a
Mustafa Şahin <mustafasahincontact@gmail.com>
2026-04-09 20:42:54 +0300

    Create page.tsx

---
+++ src/app/hakkimizda/page.tsx
@@ -0,0 +1,11 @@
+export default function Hakkimizda() {
+  return (
+    <div style={{minHeight:'100vh'}}>
+      <iframe 
+        src="https://sonusmazmatematik-github-io.vercel.app/hakkimizda.html"
+        style={{width:'100%', height:'100vh', border:'none', display:'block'}}
+        title="Hakkımızda"
+      />
+    </div>
+  );
+}
